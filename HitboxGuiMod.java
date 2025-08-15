7ba2db073fb5a9424f17b59dd2dbd51085c5e512
knowch1k <kolbasca1243@gmail.com>
2025-08-15 19:40:51 +0300

    Add files via upload

---
+++ HitboxGuiMod.java
@@ -0,0 +1,10 @@
+package com.example.hitboxgui;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HitboxGuiMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Hitbox GUI Mod loaded!");
+    }
+}
